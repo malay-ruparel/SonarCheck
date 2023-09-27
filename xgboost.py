4f5164fc9d80b0c41f93c8a1af71a45e326aede5
malay-ruparel <77070264+malay-ruparel@users.noreply.github.com>
2023-09-27 11:39:43 +0530

    Add files via upload

---
+++ xgboost.py
@@ -0,0 +1,24 @@
+import xgboost as xgb
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+# Load the Boston housing dataset
+boston = load_boston()
+X, y = boston.data, boston.target
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Define the XGBoost model
+model = xgb.XGBRegressor()
+
+# Train the model
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+mse = mean_squared_error(y_test, y_pred)
+print("Mean Squared Error:", mse)
